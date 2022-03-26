Fixed some functionality and added p normalization in newton_raphson method

common.py
@@ -18,9 +18,9 @@ def calc_grad(f, n, x_vals):
 
 def is_pos_def(matrix, n):
     def matrix_minor(arr, i, j):
-        return np.delete(np.delete(arr,i,axis=0), j, axis=1)
+        return arr[:i + 1, :j + 1]
 
-    # Все миноры должны быть положительны
+    # Все угловые миноры должны быть положительны
     for i in range(n):
         if np.linalg.det(matrix_minor(matrix, i, i)) < 0: return False
 

newton_raphson.py
@@ -13,7 +13,7 @@ def get_hessian(f, n, x_vals):
 
 
 def calc_h(grad_f, hes, p):
-    return grad_f.dot(p) / hes.dot(p).dot(p)
+    return grad_f.dot(p) / (hes.dot(p)).dot(p)
 
 
 n = 2
@@ -29,8 +29,9 @@ while True:
     # Расчет значений градиента, Гессиана и параметра h
     grad_f = calc_grad(f, n, x[-1])
     if check_conv(grad_f, eps): break
-    hes = calc_hessian(f, n, x[-1])
+    hes = get_hessian(f, n, x[-1])
     p = np.linalg.inv(hes).dot(grad_f)
+    p /= np.linalg.norm(p)
     h = calc_h(grad_f, hes, p)
 
     # Сохранение данных расчета
@@ -44,9 +45,9 @@ while True:
 
 
 # Вывод результатов
-print('i\t{: >15s}\t{: >15s}{: >20s})'.format('(x1, x2)(k)', 'f(x_k)', 'grad_f(x_k)'))
+print('i\t{: >15s}\t{: >15s}{: >20s}{: >15s}'.format('(x1, x2)(k)', 'f(x_k)', 'grad_f(x_k)', 'h_k'))
 for i in range(k):
-    print('{}:\t{: >15s}\t{: >15.4f}{: >20s}'.format(i + 1, str(x[i]), f_k[i], str(grad_k[i])))
+    print('{}:\t{: >15s}\t{: >15.4f}{: >20s}{: >15.4f}'.format(i + 1, str(x[i]), f_k[i], str(grad_k[i]), h_k[i]))
 print('\nResult: x={}\nf={}'.format(x[k], f(x[k])), end='\n\n')
 
 visualize_func_and_path(f, (-3, 3), (-8, 2), x)